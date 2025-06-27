2087b1eecf78aa6bc1d4cfe935c07dfb1a76f601
Muhammad Murtaza Hassan <136434291+murtz544@users.noreply.github.com>
2025-06-27 19:18:13 +0500

    further updates

---
+++ config/db.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { cache } from "react";
+
+let cached = global.mongoose
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function connectDB() {
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+        }
+
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/leathercart`, opts).then((mongoose) => {
+            return mongoose
+        })
+    }
+    cached.conn = await cached.promise
+    return cached.conn
+}
+
+export default connectDB;
