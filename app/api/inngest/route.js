2ec133036afb872a20dcd895eca155394abb12a5
Muhammad Murtaza Hassan <136434291+murtz544@users.noreply.github.com>
2025-06-27 19:32:53 +0500

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,12 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/config/inngest";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    // Add your Inngest functions here
-    inngest.syncUserCreation,
-    inngest.syncUserUpdation,
-    inngest.syncUserDeletion,
-  ],
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+],
 });
