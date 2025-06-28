1b4df365c5982ac504fe388e1398ff497c0678f5
Muhammad Murtaza Hassan <136434291+murtz544@users.noreply.github.com>
2025-06-28 13:11:51 +0500

    Update AppContext.jsx

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,8 +1,10 @@
 'use client'
 import { productsDummyData, userDummyData } from "@/assets/assets";
-import { useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -15,18 +17,53 @@ export const AppContextProvider = (props) => {
     const currency = process.env.NEXT_PUBLIC_CURRENCY
     const router = useRouter()
     const { user } = useUser()
+    const { getToken } = useAuth()
 
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
-    const [isSeller, setIsSeller] = useState(true)
+    const [isSeller, setIsSeller] = useState(false)
     const [cartItems, setCartItems] = useState({})
 
     const fetchProductData = async () => {
-        setProducts(productsDummyData)
+        try {
+            const { data } = await axios.get('/api/product/list')
+            if (data.success) {
+                setProducts(data.products)
+            } else {
+                toast.error(data.message);
+                console.error("Failed to fetch products:", data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+            console.error("Error fetching products:", error);
+        }
     }
 
     const fetchUserData = async () => {
-        setUserData(userDummyData)
+        try {
+            if (user.publicMetadata.role === "seller") {
+                setIsSeller(true)
+            }
+            const token = await getToken()
+            const { data } = await axios.get('/api/user/data', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            if (data.success) {
+                setUserData(data.user)
+                setCartItems(data.user.cartItems)
+            } else {
+                toast.error(data.message);
+                console.error("Failed to fetch user data:", data.message);
+            }
+
+            setUserData(userDummyData)
+        }
+        catch (error) {
+            toast.error(error.message);
+            console.error("Error fetching user data:", error);
+        }
     }
 
     const addToCart = async (itemId) => {
@@ -39,7 +76,16 @@ export const AppContextProvider = (props) => {
             cartData[itemId] = 1;
         }
         setCartItems(cartData);
-
+        if (user) {
+            try {
+                const token = await getToken();
+                await axios.post('/api/cart/update', {cartData}, {headers: { Authorization: `Bearer ${token}` } });
+                toast.success("Item added to cart");
+            } catch (error) {
+                toast.error(error.message);
+                console.error("Error adding cart:", error);
+            }
+        }
     }
 
     const updateCartQuantity = async (itemId, quantity) => {
@@ -51,7 +97,16 @@ export const AppContextProvider = (props) => {
             cartData[itemId] = quantity;
         }
         setCartItems(cartData)
-
+        if (user) {
+            try {
+                const token = await getToken();
+                await axios.post('/api/cart/update', {cartData}, {headers: { Authorization: `Bearer ${token}` } });
+                toast.success("Cart updated successfully");
+            } catch (error) {
+                toast.error(error.message);
+                console.error("Error updating cart:", error);
+            }
+        }
     }
 
     const getCartCount = () => {
@@ -80,11 +135,14 @@ export const AppContextProvider = (props) => {
     }, [])
 
     useEffect(() => {
-        fetchUserData()
-    }, [])
+        if (user)
+        {
+            fetchUserData()
+        }
+    }, [user])
 
     const value = {
-        user,
+        user, getToken,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
